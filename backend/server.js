005f12c0cede291b3fc569acfabb3d71d2f0da52
doikuomola <doikuomola@gmail.com>
2022-08-28 22:43:52 +0100

    connection to mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import data from './data.js';
+import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config();
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('connected to mongoDB');
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+connectDb();
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
